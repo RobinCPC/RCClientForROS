f72b14ba9b105cc8ca37a8f639da3396566a331d
robincpc <chienpinchen@gmail.com>
2019-04-16 17:26:16 -0700

    Update script for testing.

--- minibot_control/src/simple_mover.cpp
+++ minibot_control/src/simple_mover.cpp
@@ -20,7 +20,15 @@ auto glambda = [](std::vector<double>& v) -> std::string{
 
 void send_command(ros::Publisher& pub, ros::Rate& r)
 {
-  std::vector<double> jnt_vals = {M_PI*0.25, -1.57, 1., 1., 0.5, fabs(M_PI*-0.25)};
+  const double deg = 0.017453293;
+  std::vector<double> jnt_vals = {0., 0., 0., 0., -90*deg, 0.}; // home position
+  std::vector<std::vector<double>> jnt_arr = {};
+  jnt_arr.push_back(jnt_vals);
+  // move through 4 points
+  jnt_arr.push_back({-21.841*deg, (85.523 - 90)*deg, 5.052*deg, 0.149*deg, -90.203*deg, -21.836*deg});
+  jnt_arr.push_back({21.841*deg, (85.523 - 90)*deg, 5.052*deg, -0.149*deg, -90.203*deg, 21.841*deg});
+  jnt_arr.push_back({21.841*deg, (80.521 - 90)*deg, -34.90*deg, -0.201*deg, -45.243*deg, 21.989*deg});
+  jnt_arr.push_back({-21.841*deg, (80.521 -90)*deg, -34.906*deg, 0.201*deg, -45.242*deg, -21.985*deg});
   std_msgs::Float64MultiArray msg;
 
   msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
@@ -28,8 +36,11 @@ void send_command(ros::Publisher& pub, ros::Rate& r)
   msg.layout.dim[0].stride = 1;
   msg.layout.dim[0].label = "Joint";
 
+  int iter = 0;
   while(ros::ok())
   {
+    iter %= 5;
+    jnt_vals = jnt_arr[iter];
     //copy in the data
     ROS_INFO_STREAM("Command joint values:");
     ROS_INFO_STREAM( glambda(jnt_vals) + "\n");
@@ -37,10 +48,7 @@ void send_command(ros::Publisher& pub, ros::Rate& r)
     msg.data.insert(msg.data.end(), jnt_vals.begin(), jnt_vals.end());
     pub.publish(msg);
 
-    for(auto& j : jnt_vals)
-    {
-      j *= -1;
-    }
+    ++iter;
     r.sleep();
   }
 
@@ -52,7 +60,7 @@ int main(int argc, char *argv[])
   // initialize
   ros::init(argc, argv, "simple_mover");
   ros::NodeHandle nh;
-  ros::Rate rate = ros::Rate(0.1);
+  ros::Rate rate = ros::Rate(0.5);
 
   ros::Publisher pub_group = nh.advertise<std_msgs::Float64MultiArray>
     ("/minibot/joint_position_controller/command",  5);
